ec97b9e97831b45d6de646f0d4a2fbc67e39cea8
liangtf <liangtf@gmail.com>
2016-12-29 19:52:52 +0800

    nothing changed

--- SpriteKitSimpleGame/GameScene.swift
+++ SpriteKitSimpleGame/GameScene.swift
@@ -57,9 +57,7 @@ class GameScene: SKScene  {
         // add it to scene
         addChild(house)
         
-//        physicsWorld.gravity = CGVector.zero
-//        physicsWorld.contactDelegate = self
-        
+
         run(SKAction.repeatForever(
             SKAction.sequence([
                 SKAction.run(addTank),
@@ -188,9 +186,13 @@ class GameScene: SKScene  {
         
         let actionMoveDone = SKAction.removeFromParent()
         let loseAction = SKAction.run() {
+            // points decrease
 //            let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
 //            let gameOverScene = GameOverScene(size: self.size, won: false)
 //            self.view?.presentScene(gameOverScene, transition: reveal)
+            // check points
+            
+            //
         }
         monster.run(SKAction.sequence([actionMove, loseAction, actionMoveDone]))
         
@@ -222,13 +224,6 @@ class GameScene: SKScene  {
         
         // Add the monster to the scene
         addChild(monster)
-        
-        //        monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size) // 1
-        //        monster.physicsBody?.isDynamic = true // 2
-        //        monster.physicsBody?.categoryBitMask = PhysicsCategory.Monster // 3
-        //        monster.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile // 4
-        //        monster.physicsBody?.collisionBitMask = PhysicsCategory.None // 5
-        
         
         // Determine speed of the monster
         let actualDuration = random(min: CGFloat(3.0), max: CGFloat(4.0))
@@ -271,13 +266,6 @@ class GameScene: SKScene  {
         
         // Add the monster to the scene
         addChild(monster)
-        
-        //        monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size) // 1
-        //        monster.physicsBody?.isDynamic = true // 2
-        //        monster.physicsBody?.categoryBitMask = PhysicsCategory.Monster // 3
-        //        monster.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile // 4
-        //        monster.physicsBody?.collisionBitMask = PhysicsCategory.None // 5
-        
         
         // Determine speed of the monster
         let actualDuration = random(min: CGFloat(7.0), max: CGFloat(8.0))
@@ -322,86 +310,6 @@ class GameScene: SKScene  {
             }
         }
     }
-    
-//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        
-//        run(SKAction.playSoundFileNamed("pew-pew-lei.caf", waitForCompletion: false))
-//        
-//        // 1 - Choose one of the touches to work with
-//        guard let touch = touches.first else {
-//            return
-//        }
-//        let touchLocation = touch.location(in: self)
-//        
-//        // 2 - Set up initial location of projectile
-//        let projectile = SKSpriteNode(imageNamed: "projectile")
-//        projectile.position = player.position
-//        
-//        // 3 - Determine offset of location to projectile
-//        let offset = touchLocation - projectile.position
-//        
-//        // 4 - Bail out if you are shooting down or backwards
-//        if (offset.x < 0) { return }
-//        
-//        // 5 - OK to add now - you've double checked position
-//        addChild(projectile)
-//        
-//        // 6 - Get the direction of where to shoot
-//        let direction = offset.normalized()
-//        
-//        // 7 - Make it shoot far enough to be guaranteed off screen
-//        let shootAmount = direction * 1000
-//        
-//        // 8 - Add the shoot amount to the current position
-//        let realDest = shootAmount + projectile.position
-//        
-//        projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width/2)
-//        projectile.physicsBody?.isDynamic = true
-//        projectile.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
-//        projectile.physicsBody?.contactTestBitMask = PhysicsCategory.Monster
-//        projectile.physicsBody?.collisionBitMask = PhysicsCategory.None
-//        projectile.physicsBody?.usesPreciseCollisionDetection = true
-//        
-//        // 9 - Create the actions
-//        let actionMove = SKAction.move(to: realDest, duration: 2.0)
-//        let actionMoveDone = SKAction.removeFromParent()
-//        projectile.run(SKAction.sequence([actionMove, actionMoveDone]))
-//        
-//    }
-    
-//    func didBegin(_ contact: SKPhysicsContact) {
-//        
-//        // 1
-//        var firstBody: SKPhysicsBody
-//        var secondBody: SKPhysicsBody
-//        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
-//            firstBody = contact.bodyA
-//            secondBody = contact.bodyB
-//        } else {
-//            firstBody = contact.bodyB
-//            secondBody = contact.bodyA
-//        }
-//        
-//        // 2
-//        if ((firstBody.categoryBitMask & PhysicsCategory.Monster != 0) &&
-//            (secondBody.categoryBitMask & PhysicsCategory.Projectile != 0)) {
-//            projectileDidCollideWithMonster(projectile: firstBody.node as! SKSpriteNode, monster: secondBody.node as! SKSpriteNode)
-//        }
-//        
-//    }
-    
-//    func projectileDidCollideWithMonster(projectile: SKSpriteNode, monster: SKSpriteNode) {
-//        print("Hit")
-//        projectile.removeFromParent()
-//        monster.removeFromParent()
-//        monstersDestroyed += 1
-//        if (monstersDestroyed > 2) {
-//            let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
-//            let gameOverScene = GameOverScene(size: self.size, won: true)
-//            self.view?.presentScene(gameOverScene, transition: reveal)
-//        }
-//    }
-    
     
 }
 
